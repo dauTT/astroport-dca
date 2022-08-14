a57cf7c20df32d301b6d4c0159abc8e3c4c93d87
DauTT <dautruongtan@gmail.com>
2022-08-14 17:49:34 +0530

    Refactor snippet.ts

--- scripts/tests/snippet.ts
+++ scripts/tests/snippet.ts
@@ -17,9 +17,7 @@ import {
   NativeAsset,
   TokenAsset,
 } from "../helpers.js";
-import { join } from "path";
-
-import { logToFile, deleteFile, LOGS_PATH } from "../util.js";
+import * as fs from "fs";
 
 import { initTestClient } from "./common.js";
 
@@ -40,6 +38,8 @@ import {
   Wallet,
 } from "@terra-money/terra.js";
 
+import { LOCAL_TERRA_TEST_ACCOUNTS } from "../util.js";
+
 // This is a kind of playground for designing concrete tests.
 // Create small runnable snippet here till you can aggregated them into a test
 async function main() {
@@ -48,58 +48,79 @@ async function main() {
     "test1"
   );
 
-  let queryName: String;
-  let query: any;
-
-  queryName = "config dca";
-  query = {
-    config: {},
-  };
-
-  let res = await queryContractDebug(
+  await queryBankDebug(terra, network.DcaAddress, " luna balance", logPath);
+  await get_token_balance(
     terra,
-    network.DcaAddress,
-    query,
-    queryName,
+    network.tokenAddresses.AAA,
+    LOCAL_TERRA_TEST_ACCOUNTS["test1"].addr,
     logPath
   );
   /*
-  queryName = "pair: AAA-BBB";
-  query = {
+  await get_dca_config(terra, logPath, network);
+  await get_dca_order_id(terra, "1", logPath, network);
+  await get_reply_sub_msg(terra, logPath, network);
+
+  await get_dca_config(terra, logPath, network);
+  await query_pool(
+    terra,
+    new TokenAsset(network.tokenAddresses.AAA).getInfo(),
+    new TokenAsset(network.tokenAddresses.BBB).getInfo(),
+    logPath,
+    network
+  );
+*/
+}
+
+main().catch(console.log);
+
+async function query_pool(
+  terra: LCDClient,
+  asset1_info: any,
+  asset2_info: any,
+  logPath: fs.PathOrFileDescriptor,
+  network: any
+): Promise<any> {
+  let queryName = "Query pool";
+  let query = {
     pair: {
-      asset_infos: [
-        new TokenAsset(network.tokenAddresses.AAA).getInfo(),
-        new TokenAsset(network.tokenAddresses.BBB).getInfo(),
-      ],
+      asset_infos: [asset1_info, asset2_info],
     },
   };
 
-  res = await queryContractDebug(
+  return await queryContractDebug(
     terra,
     network.factoryAddress,
     query,
     queryName,
     logPath
   );
+}
 
-*/
-
-  queryName = "lunabalance dca address";
-  await queryBankDebug(terra, network.DcaAddress, queryName, logPath);
-
-  queryName = "dca_orders with id =2 ";
-  query = { dca_orders: { id: "2" } };
-  await queryContractDebug(
+async function get_token_balance(
+  terra: LCDClient,
+  contract_addr: string,
+  user_addr: string,
+  logPath: fs.PathOrFileDescriptor
+): Promise<any> {
+  let queryName = `Get balance of token = ${contract_addr} for use=${user_addr} `;
+  let query = { balance: { address: user_addr } };
+  return await queryContractDebug(
     terra,
-    network.DcaAddress,
+    contract_addr,
     query,
     queryName,
     logPath
   );
+}
 
-  queryName = "sub_msg ";
-  query = { reply_sub_msg_response: {} };
-  await queryContractDebug(
+async function get_reply_sub_msg(
+  terra: LCDClient,
+  logPath: fs.PathOrFileDescriptor,
+  network: any
+): Promise<any> {
+  let queryName = "sub_msg ";
+  let query = { reply_sub_msg_response: {} };
+  return await queryContractDebug(
     terra,
     network.DcaAddress,
     query,
@@ -108,4 +129,38 @@ async function main() {
   );
 }
 
-main().catch(console.log);
+async function get_dca_config(
+  terra: LCDClient,
+  logPath: fs.PathOrFileDescriptor,
+  network: any
+): Promise<any> {
+  let queryName = "config dca";
+  let query = {
+    config: {},
+  };
+
+  return await queryContractDebug(
+    terra,
+    network.DcaAddress,
+    query,
+    queryName,
+    logPath
+  );
+}
+
+async function get_dca_order_id(
+  terra: LCDClient,
+  id: string,
+  logPath: fs.PathOrFileDescriptor,
+  network: any
+): Promise<any> {
+  let queryName = `dca_orders with id = ${id} `;
+  let query = { dca_orders: { id: id } };
+  return await queryContractDebug(
+    terra,
+    network.DcaAddress,
+    query,
+    queryName,
+    logPath
+  );
+}
